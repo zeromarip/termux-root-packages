4c225e53f51dda44aac43edd6c9abf7f3af86250
Henrik Grimler <Henrik@Grimler.se>
2018-10-14 15:59:12 +0200

    libusb: add android patch

--- packages/libusb/build.sh
+++ packages/libusb/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://libusb.info/
 TERMUX_PKG_DESCRIPTION="A cross-platform user library to access USB devices"
 TERMUX_PKG_VERSION=1.0.22
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libusb/libusb/archive/v${TERMUX_PKG_VERSION}.zip
 TERMUX_PKG_SHA256=cb10afc04399f5aa15cce246e5f043bea3547d128f088d62874039f984db7879
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-udev"
